792449e9f8778548133bd3ea33aff0eb064fd0c1
tguankheng016 <tguankheng@outlook.com>
2025-01-20 22:59:29 +0800

    Amend Initial Data

--- src/OpenIddictOAuth.Web/Data/Seed/InitialData.cs
+++ src/OpenIddictOAuth.Web/Data/Seed/InitialData.cs
@@ -26,10 +26,38 @@ public static class InitialData
         {
             new OpenIddictApplicationDescriptor
             {
-                ClientId = "flight-booking",
+                ClientId = "dotnet-commerce",
                 ClientSecret = "901564A5-E7FE-42CB-B10D-61EF6A8F3654",
                 ConsentType = ConsentTypes.Explicit,
-                DisplayName = "Flight booking microservice application",
+                DisplayName = "Dotnet Commerce Micro",
+                RedirectUris =
+                {
+                    new Uri("http://localhost:4200/account/callback/login"),
+                    new Uri("http://localhost:5173/account/callback/login")
+                },
+                PostLogoutRedirectUris =
+                {
+                    new Uri("https://localhost:4200/account/callback/logout"),
+                    new Uri("https://localhost:5173/account/callback/logout")
+                },
+                Permissions =
+                {
+                    Permissions.Endpoints.Authorization,
+                    Permissions.Endpoints.Logout,
+                    Permissions.Endpoints.Token,
+                    Permissions.GrantTypes.AuthorizationCode,
+                    Permissions.ResponseTypes.Code,
+                    Permissions.Scopes.Email,
+                    Permissions.Scopes.Profile,
+                    Permissions.Scopes.Roles
+                }
+            },
+            new OpenIddictApplicationDescriptor
+            {
+                ClientId = "go-commerce",
+                ClientSecret = "901564A5-E7FE-42CB-B10D-61EF6A8F3654",
+                ConsentType = ConsentTypes.Explicit,
+                DisplayName = "Go Commerce Micro",
                 RedirectUris =
                 {
                     new Uri("http://localhost:4200/account/callback/login"),
